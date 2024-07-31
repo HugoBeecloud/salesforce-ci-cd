c65a06194e17b06bc1a4cf12d88b107a2670f55c
manzolimatheus <manzoli.contato@gmail.com>
2024-07-31 15:01:33 -0300

    project start

---
+++ .github/pull_request_template.md
@@ -0,0 +1,16 @@
+# Resumo da tarefa *
+...
+  
+# 🔥 Mudanças *
+...
+
+# 🤖 Classes de teste para serem executadas
+
+Apex::[Class1,Class2,Class3...]::Apex
+  
+# 👀 Informações Adicionais
+
+💻 Tarefa Relacionada: []
+✔ Aprovado pelo cliente: []
+
+## 💬 Notas/Histórico
---
+++ .github/scripts/generate_comment.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+const filePath = "changed-sources/apexScanResults.json";
+
+if (fs.existsSync(filePath)) {
+  const jsonData = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(jsonData);
+
+  let markdownContent = "";
+
+  // Iterar sobre cada entrada no JSON
+  data.forEach((entry) => {
+    const engine = entry.engine;
+    const fileName = entry.fileName;
+
+    const forceAppIndex = fileName.indexOf("force-app");
+    if (forceAppIndex !== -1) {
+      const pathAfterForceApp = fileName.substring(forceAppIndex);
+
+      markdownContent += `### Análise Estática de Código - ${engine}\n`;
+      markdownContent += `Arquivo: ${pathAfterForceApp}\n\n`;
+    } else {
+      markdownContent += `### Análise Estática de Código - ${engine}\n`;
+      markdownContent += `Arquivo: ${fileName}\n\n`;
+    }
+
+    const violations = entry.violations;
+
+    if (violations.length > 0) {
+      markdownContent += "#### Detalhes das Violações:\n";
+      violations.forEach((violation, index) => {
+        markdownContent += `${index + 1}. **Regra:** ${violation.ruleName}\n`;
+        markdownContent += `   - **Gravidade:** ${violation.severity}\n`;
+        markdownContent += `   - **Categoria:** ${violation.category}\n`;
+        markdownContent += `   - **Descrição:** ${violation.message.trim()}\n`;
+        markdownContent += `   - **Localização:** Linha ${violation.line}, Coluna ${violation.column} até Linha ${violation.endLine}, Coluna ${violation.endColumn}\n`;
+        markdownContent += `   - **[Link para Documentação](${violation.url})**\n\n`;
+      });
+    } else {
+      markdownContent += "Não foram encontradas violações neste arquivo.\n\n";
+    }
+
+    // Adicionar separador entre diferentes entradas, se houver mais de uma
+    markdownContent += "---\n\n";
+  });
+
+  // Escrever o conteúdo Markdown em um arquivo .md
+  fs.writeFileSync("comment.md", markdownContent);
+} else {
+  console.error(`Arquivo não encontrado: ${filePath}`);
+}
